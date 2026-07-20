87f64c0c7131c1f7d4cbe651a2d64fe6c9eeb25d
berzasgilad <berzasgilad@gmail.com>
2026-07-20 18:21:42 +0300

    using cd first time

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@
 #include <iostream>
 
 int main() {
-    std::couut << "This is my new feature! 11111111" << std::endl;
+    std::cout << "This is my new feature! 11111111" << std::endl;
     return 0;
 }
